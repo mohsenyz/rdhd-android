d5f6d1a5d44eaeb9c70512639d4694f4f858970b
mohsenataei <mohsenataeiasad@gmail.com>
2019-09-21 04:08:46 +0430

    transaction icon changed.

---
+++ app/src/main/java/com/rdhd/app/ui/PaymentsActivity.kt
@@ -0,0 +1,12 @@
+package com.rdhd.app.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PaymentsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_payments)
+    }
+}
