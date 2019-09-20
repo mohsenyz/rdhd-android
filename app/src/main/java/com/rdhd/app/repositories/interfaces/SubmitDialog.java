170177b4ea3797219caeb2259570c98510634caa
mohsenataei <mohsenataeiasad@gmail.com>
2019-09-21 00:20:45 +0430

    submit dialog interface for returning data from dialog to activity added.

---
+++ app/src/main/java/com/rdhd/app/repositories/interfaces/SubmitDialog.java
@@ -0,0 +1,7 @@
+package com.rdhd.app.repositories.interfaces;
+
+import com.rdhd.app.models.ProviderService;
+
+public interface SubmitDialog {
+    public void OnSubmitListener(ProviderService providerService);
+}
